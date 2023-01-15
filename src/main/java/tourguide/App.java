2757c73ad6e6f13468818e8b14d51bcb8c12f3ce
stellaratlas8 <ajmalinao1@gmail.com>
2023-01-15 10:48:31 +0800

    Cleanup

--- src/main/java/tourguide/App.java
+++ src/main/java/tourguide/App.java
@@ -11,29 +11,28 @@ public class App {
     Scanner scan = new Scanner(System.in);
 
     Area[] areas = {
-        new Area("San Pablo")
-            .spots(new TouristSpot("Sampaloc Lake"), new TouristSpot("Lake Pandin"))
-            .hotels(new Hotel("Casa San Pablo", "none", 200))
+            new Area("San Pablo")
+                    .spots(new TouristSpot("Sampaloc Lake"), new TouristSpot("Lake Pandin"))
+                    .hotels(new Hotel("Casa San Pablo", "none", 200))
     };
     Area selectedArea;
-    ArrayList<TouristSpot> destinations;
+    ArrayList<TouristSpot> destinations = new ArrayList<>();
     Hotel hotel;
     double cost;
 
-    public void run(){
+    public void run() {
         // Opening screen
-        while (selectedArea == null){
+        while (selectedArea == null) {
             clear();
             println("Welcome to the tour guide planning program!");
             println("To begin, please select an area by typing the name of the area below\nAreas\n");
-            for(Area area : areas){
+            for (Area area : areas) {
                 printf("%s\n", area.name);
             }
-            print("\n");
-            print(" : ");
+            print("\n : ");
             String input = scan.nextLine().toLowerCase();
-            for(Area area : areas){
-                if(area.name.toLowerCase().equals(input)){
+            for (Area area : areas) {
+                if (area.name.toLowerCase().equals(input)) {
                     selectedArea = area;
                     break;
                 }
@@ -42,17 +41,17 @@ public class App {
 
         // Location selection
         boolean selecting = true;
-        while (selecting){
+        while (selecting) {
             clear();
             println("These are the top destinations in the area.\n");
-            for(TouristSpot spot : selectedArea.spots){
+            for (TouristSpot spot : selectedArea.spots) {
                 printf("\t%s\n\n%s\n", spot.name, spot.description);
             }
             print("\n");
             print(" : ");
             String input = scan.nextLine().toLowerCase();
-            for(TouristSpot spot : selectedArea.spots){
-                if(spot.name.toLowerCase().equals(input)){
+            for (TouristSpot spot : selectedArea.spots) {
+                if (spot.name.toLowerCase().equals(input)) {
                     destinations.add(spot);
                     selecting = false;
                     break;
@@ -61,25 +60,25 @@ public class App {
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         App app = new App();
-        
+
         app.run();
     }
 
-    public static void print(String str){
+    public static void print(String str) {
         System.out.print(str);
     }
 
-    public static void printf(String str, Object... arg){
+    public static void printf(String str, Object... arg) {
         System.out.printf(str, arg);
     }
 
-    public static void println(String str){
+    public static void println(String str) {
         System.out.println(str);
     }
 
-    public static void clear(){
+    public static void clear() {
         System.out.print("\033[2J\033[1;1H");
     }
 }
